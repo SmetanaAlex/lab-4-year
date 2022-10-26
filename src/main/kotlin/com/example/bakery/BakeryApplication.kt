2d146b1af25ca0eb0853c56fdb2e88de24126cc8
SmetanaAlex <bankasmetana@gmail.com>
2022-10-26 21:10:38 +0300

    init comit

---
+++ src/main/kotlin/com/example/bakery/BakeryApplication.kt
@@ -0,0 +1,11 @@
+package com.example.bakery
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BakeryApplication
+
+fun main(args: Array<String>) {
+    runApplication<BakeryApplication>(*args)
+}
