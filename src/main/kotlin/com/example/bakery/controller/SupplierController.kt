debf8ad16092f5f768ae7c6f77e4018516ca6e12
SmetanaAlex <bankasmetana@gmail.com>
2022-10-29 15:17:53 +0300

    supplier supply

--- src/main/kotlin/com/example/bakery/controller/SupplierController.kt
+++ src/main/kotlin/com/example/bakery/controller/SupplierController.kt
@@ -1,10 +1,9 @@
 package com.example.bakery.controller
 
 import com.example.bakery.dto.supplier.SupplierCategoryVariationResponse
+import com.example.bakery.dto.supplier.SupplierSupplyRequest
 import com.example.bakery.service.SupplierService
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping(path = ["/supplier/products"])
@@ -12,5 +11,10 @@ class SupplierController(val supplierService: SupplierService) {
     @GetMapping("/category-variation")
     fun showCategoryVariation(): List<SupplierCategoryVariationResponse> {
         return supplierService.showCategoryVariation()
+    }
+
+    @PostMapping("/supply")
+    fun supply(@RequestBody request: List<SupplierSupplyRequest>) {
+        supplierService.supply(request)
     }
 }
